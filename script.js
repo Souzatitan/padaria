40601fc63e29f2b9970be83f307cf2acf31f726b
Souzatitan <asouzanderson@gmail.com>
2024-06-13 21:04:21 -0300

    responsivo

---
+++ script.js
@@ -0,0 +1,8 @@
+document.getElementById('toggle-button').addEventListener('click', function () {
+    var navbarLinks = document.getElementById('navbar-links');
+    if (navbarLinks.style.display === 'flex') {
+        navbarLinks.style.display = 'none';
+    } else {
+        navbarLinks.style.display = 'flex';
+    }
+});
